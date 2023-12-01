46e31daa10de8aed3500c692efd428f041cfc47e
VexiDev <79677251+VexiDev@users.noreply.github.com>
2023-12-01 16:31:02 -0500

    implemented discord login

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, redirect, request, session, jsonify, render_template, url_for
+from requests_oauthlib import OAuth2Session
+import os
+from dotenv import load_dotenv
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'  # Only for development!
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)  # Replace with a real secret key
+
+
+
+# These values should be read from a config file or environment variables
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+SCOPE = ['identify', 'guilds']
+
+discord = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
+
+@app.route('/')
+def home():
+    return render_template('home_1.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/login')
+def login():
+    authorization_url, state = discord.authorization_url('https://discord.com/api/oauth2/authorize')
+    session['oauth2_state'] = state
+    return redirect(authorization_url)
+
+@app.route('/callback')
+def callback():
+    discord.fetch_token('https://discord.com/api/oauth2/token',
+                        client_secret=CLIENT_SECRET,
+                        authorization_response=request.url)
+    user_data = discord.get('https://discord.com/api/users/@me').json()
+    user_profile = UserProfile(user_data)
+    session['user_info'] = {
+        'username': user_profile.username,
+        'avatar_url': user_profile.avatar_url,
+        'id': user_profile.id
+    }
+    return redirect(url_for('home'))
+
+@app.route('/check_login')
+def check_login():
+    user_info = session.get('user_info', None)
+    return jsonify({
+        'logged_in': user_info is not None,
+        'user_info': user_info or {}
+    })
+
+class UserProfile:
+    def __init__(self, user_data):
+        self.id = user_data.get('id')
+        self.username = user_data.get('username')
+        self.avatar_hash = user_data.get('avatar')
+        self.discriminator = user_data.get('discriminator')
+        self.public_flags = user_data.get('public_flags')
+        self.premium_type = user_data.get('premium_type')
+        self.flags = user_data.get('flags')
+        self.banner_hash = user_data.get('banner')
+        self.accent_color = user_data.get('accent_color')
+        self.global_name = user_data.get('global_name')
+        self.avatar_decoration_data = user_data.get('avatar_decoration_data')
+        self.banner_color = user_data.get('banner_color')
+        self.mfa_enabled = user_data.get('mfa_enabled')
+        self.locale = user_data.get('locale')
+
+    @property
+    def avatar_url(self):
+        if self.avatar_hash:
+            return f"https://cdn.discordapp.com/avatars/{self.id}/{self.avatar_hash}.png"
+        else:
+            return None
+
+    @property
+    def banner_url(self):
+        if self.banner_hash:
+            return f"https://cdn.discordapp.com/banners/{self.id}/{self.banner_hash}.png"
+        else:
+            return None
+
+if __name__ == '__main__':
+    app.run(port=4200)
